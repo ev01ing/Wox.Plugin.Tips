a47d5779e6363a96ee0bb0b1cd32b2d66ec205e4
ev01ing <evolingyoung@qq.com>
2018-08-25 14:32:04 +0800

    修改readme, 增加中文不显示的解决办法

--- main.py
+++ main.py
@@ -4,108 +4,120 @@ import time
 import json
 import traceback
 import clipboard
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 class Main(Wox):
-	LOG_FILE = "./logs/error.txt"
-	TIPS_FILE = "./DB/tips.json"
-	def query(self, key):
-		if key == "":
-			return self.list_tips()
-		if key in "delete":
-			return self.list_delete_tips()
+    LOG_FILE = "./logs/error.txt"
+    TIPS_FILE = "./DB/tips.json"
 
-		results = []
-		results.append({
-			"Title": "stroe tip : %s" % key,
-			"SubTitle": u"tips: %s" % key,
-			"IcoPath": "Images/pic.png",
-			"JsonRPCAction":{
-				"method": "store_tip",
-				"parameters": [key, ],
-				"dontHideAfterAction": False
-			}
-		})
-		return results
+    def query(self, key):
 
-	def store_tip(self, info):
-		try:
-			times = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-			tip = {"tip": info, "updated_time": times}
-			with open(self.TIPS_FILE, "a") as f:
-				f.write(json.dumps(tip) + "\n")
-		except:
-			self.log_error("store_tip")
+        with open("temp.txt", "a") as output:
+            output.write(key + "\n")
 
+        if key == "":
+            return self.list_tips()
+        if key in "delete":
+            return self.list_delete_tips()
 
-	def get_tips(self):
-		try:
-			results = []
-			with open(self.TIPS_FILE, "r") as f:
-				for line in f:
-					tip = json.loads(line.strip())
-					results.append({"tip": tip['tip'], "updated_time": tip['updated_time']})
-			return reversed(results)
-		except:
-			self.log_error("get_tips")
+        results = []
+        results.append({
+            "Title": u"stroe tip : %s" % key,
+            "SubTitle": u"tips: %s" % key,
+            "IcoPath": "Images/pic.png",
+            "JsonRPCAction": {
+                "method": "store_tip",
+                "parameters": [key, ],
+                "dontHideAfterAction": False
+            }
+        })
 
-	def list_delete_tips(self):
-		tips = self.get_tips()
-		results = []
-		for tip in tips:
-			results.append({
-				"Title": "delete %s" % tip['tip'],
-				"SubTitle": tip['updated_time'],
-				"IcoPath": "Images/pic.png",
-				"JsonRPCAction": {
-					"method": "delete_tip",
-					"parameters": [tip['updated_time']],
-					"dontHideAfterAction": False
-				}
-			})
-		return results
+        return results
 
-	def delete_tip(self, times):
-		try:
-			tips = []
-			with open(self.TIPS_FILE, "r") as f:
-				for line in f:
-					tips.append(json.loads(line.strip()))
-			for tip in tips:
-				if times == tip["updated_time"]:
-					tips.remove(tip)
-					break
-			with open(self.TIPS_FILE, "w") as f:
-				for tip in tips:
-					f.write(json.dumps(tip) + "\n")
-		except:
-			self.log_error("delete_tip")
+    def store_tip(self, info):
+        try:
+            times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            tip = {"tip": info, "updated_time": times}
+            with open(self.TIPS_FILE, "a") as f:
+                f.write(json.dumps(tip) + "\n")
+        except:
+            self.log_error("store_tip")
 
-	def list_tips(self):
-		tips = self.get_tips()
-		results = []
-		for tip in tips:
-			results.append({
-				"Title": tip['tip'],
-				"SubTitle": tip['updated_time'],
-				"IcoPath": "Images/pic.png",
-				"JsonRPCAction": {
-					"method": "copy_to_clip",
-					"parameters": [tip['tip']],
-					"dontHideAfterAction": False
-				}
-			})
-		with open("temp.txt", "w") as f:
-			for item in results:
-				f.write(json.dumps(item) + "\n")
-		return results
+    def get_tips(self):
+        try:
+            results = []
+            with open(self.TIPS_FILE, "r") as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        tip = json.loads(line.strip())
+                        results.append({"tip": tip['tip'], "updated_time": tip['updated_time']})
+            return reversed(results)
+        except:
+            self.log_error("get_tips")
 
-	def copy_to_clip(self, text):
-		clipboard.copy(text)
+    def list_delete_tips(self):
+        tips = self.get_tips()
+        results = []
+        for tip in tips:
+            results.append({
+                "Title": "delete %s" % tip['tip'],
+                "SubTitle": tip['updated_time'],
+                "IcoPath": "Images/pic.png",
+                "JsonRPCAction": {
+                    "method": "delete_tip",
+                    "parameters": [tip['updated_time']],
+                    "dontHideAfterAction": False
+                }
+            })
+        return results
 
-	def log_error(self, fun_str):
-		with open(self.LOG_FILE, "a") as f:
-			f.write("%s: \n%s" % (fun_str, traceback.format_exc()))
+    def delete_tip(self, times):
+        try:
+            tips = []
+            with open(self.TIPS_FILE, "r") as f:
+                for line in f:
+                    tips.append(json.loads(line.strip()))
+            for tip in tips:
+                if times == tip["updated_time"]:
+                    tips.remove(tip)
+                    break
+            with open(self.TIPS_FILE, "w") as f:
+                for tip in tips:
+                    f.write(json.dumps(tip) + "\n")
+        except:
+            self.log_error("delete_tip")
+
+    def list_tips(self):
+        tips = self.get_tips()
+        results = []
+        for tip in tips:
+            results.append({
+                "Title": tip['tip'],
+                "SubTitle": tip['updated_time'],
+                "IcoPath": "Images/pic.png",
+                "JsonRPCAction": {
+                    "method": "copy_to_clip",
+                    "parameters": [tip['tip']],
+                    "dontHideAfterAction": False
+                }
+            })
+        with open("temp.txt", "a") as f:
+            for item in results:
+                f.write(json.dumps(item) + "\n")
+        return results
+
+    def copy_to_clip(self, text):
+        clipboard.copy(text)
+
+    def log_error(self, fun_str):
+        with open(self.LOG_FILE, "a") as f:
+            f.write("%s: \n%s" % (fun_str, traceback.format_exc()))
+
 
 if __name__ == "__main__":
-	Main()
+    Main()
--- readme.md
+++ readme.md
@@ -7,26 +7,18 @@ wpm install tips
 
 
 #### 使用说明
-![tip](https://raw.githubusercontent.com/ev01ing/Wox.Plugin.Tips/master/docs/pics/2.PNG)
+![tip](https://raw.githubusercontent.com/ev01ing/Wox.Plugin.Tips/master/docs/pics/wox.gif)
 ```
-tip 
-```
-将会列出所有的tip，移动到对应的tip下，回车可复制tip到粘帖板
 
-
-![store tip](https://raw.githubusercontent.com/ev01ing/Wox.Plugin.Tips/master/docs/pics/1.PNG)
-```
-tip little idea
-```
-回车store tip将存储tip
-
-
-![delete tip](https://raw.githubusercontent.com/ev01ing/Wox.Plugin.Tips/master/docs/pics/3.PNG)
-```
-tip delete
-```
-选择对应的tip回车即可
 
 #### python依赖
 
-插件使用了time, clipboard, json, clipboard包，使用前请安装依赖包
+插件使用了time, json, clipboard包，使用前请安装依赖包
+
+
+#### 中文无法使用的解决办法
+
+1. 找到系统的默认字符集，一般为gbk
+2. 找到wox.py的位置，安装目录下的。 可参考 C:\Users\users\AppData\Local\Wox\app-1.3.524\JsonRPC 
+3. 将`class wox`的`__init__`第一行改为
+`rpc_request = json.loads(sys.argv[1].decode("gbk").encode("utf-8"))`
